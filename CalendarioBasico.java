8324791f6b1d4ca5af6a3ac444c07ba6d21d85d8
Lorena Alonso <lorenaalonsop26@gmail.com>
2017-10-30 18:04:57 +0100

    Creado constructor

---
+++ CalendarioBasico.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class CalendarioBasico here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CalendarioBasico
+{
+    // instance variables - replace the example below with your own
+    private int day;
+    private int month;
+    private int year;
+
+    /**
+     * Constructor for objects of class CalendarioBasico
+     */
+    public CalendarioBasico()
+    {
+        // initialise instance variables
+        day = 01;
+        month = 01;
+        year = 01;
+    }
+
+}
